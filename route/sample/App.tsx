09572db6dbb2014b6227d44b5f9df8157f4da7aa
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2021-10-27 15:12:49 +0900

    Update App.tsx

--- route/sample/App.tsx
+++ route/sample/App.tsx
@@ -1,1 +1,16 @@
-
+import * as React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import Home from "./router/Home";
+import PageA from "./router/PageA";
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/page_a" exact component={PageA} />
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+export default App;
