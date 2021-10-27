09cd8f914272df6aadcf98c00b3c93719fa6d8af
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2021-10-27 15:13:23 +0900

    Update Home.tsx

--- route/sample/route/Home.tsx
+++ route/sample/route/Home.tsx
@@ -1,1 +1,11 @@
+import * as React from 'react';
+import { Link } from "react-router-dom";
 
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link to="/page_a">PageA</Link>
+    </div>
+  );
+}
