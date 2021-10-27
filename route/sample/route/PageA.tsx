55cb18599d55261eb20298c2649d451534b46114
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2021-10-27 15:13:52 +0900

    Update PageA.tsx

--- route/sample/route/PageA.tsx
+++ route/sample/route/PageA.tsx
@@ -1,1 +1,11 @@
+import * as React from 'react';
+import { Link } from "react-router-dom";
 
+export default function PageA() {
+  return (
+    <div>
+      <h1>PageA</h1>
+      <Link to="/">Home</Link>
+    </div>
+  );
+}
