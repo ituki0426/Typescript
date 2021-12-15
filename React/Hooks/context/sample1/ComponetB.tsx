fc38ce201c251a5da4f0ae6c303e7c62e0fa04b0
ituki0426 <82156802+ituki0426@users.noreply.github.com>
2021-12-15 19:11:32 +0900

    Update ComponetB.tsx

--- React/Hooks/context/sample1/ComponetB.tsx
+++ React/Hooks/context/sample1/ComponetB.tsx
@@ -1,1 +1,16 @@
+import * as React from "react";
+import { SampleTextContext } from "./context";
 
+const MyFunctionalComponents:React.FC = () => {
+    return (
+        <SampleTextContext.Consumer>
+            {value => {
+                return(
+                    <div>{value}</div>
+                )
+            }}
+        </SampleTextContext.Consumer>
+    )
+}
+
+export default MyFunctionalComponents;
